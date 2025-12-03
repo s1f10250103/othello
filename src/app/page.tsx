bac8a6b9828c35c680827b189a135c9abc54dc58
s1f10250103 <s1f102501035@iniad.org>
2025-12-03 14:07:57 +0900

    while文を作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,16 +28,30 @@ export default function Home() {
 
   const [turn, setTurn] = useState(1);
 
-  for (const direction of directions) {
-    const dx = direction[0]; // X方向の移動量
-    const dy = direction[1]; // Y方向の移動量
-  }
-
   const handleCellClick = (rowIndex: number, cellIndex: number) => {
     const newBoard = structuredClone(board);
     newBoard[rowIndex][cellIndex] = turn;
     setTurn(turn === 1 ? 2 : 1);
     setBoard(newBoard);
+    for (const direction of directions) {
+      const dx = direction[0];
+      const dy = direction[1];
+      const x = cellIndex + dx;
+      const y = rowIndex + dy;
+      const stonesToFlip = [];
+      while (x >= 0 && x < 8 && y >= 0 && y < 8) {
+        const color = newBoard[y][x];
+        if (color === 0) {
+          break;
+        }
+        if (color === turn) {
+          if (stonesToFlip.length > 0) {
+            // ここで裏返し処理を実行
+          }
+          break;
+        }
+      }
+    }
   };
 
   return (
